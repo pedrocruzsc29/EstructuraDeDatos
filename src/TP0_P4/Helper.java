6bc7eb6762bc9ece93d4eda98aafeaf8c2bc1c49
Pedro Cruz <pedrocruzsc2@gmail.com>
2025-08-21 04:34:17 -0300

    Actualizar Helper.java y TP0_P4.java

--- src/TP0_P4/Helper.java
+++ src/TP0_P4/Helper.java
@@ -1,4 +1,25 @@
 package TP0_P4;
 
+import java.util.Scanner;
+
 public class Helper {
+
+    public static Scanner sc = new Scanner(System.in);
+    public static int GetValidNumber (String message){
+        boolean band = false;
+        int number = 0;
+        System.out.println(message);
+        String entrance = sc.nextLine().trim();
+        do {
+            if (entrance.isEmpty()){
+                System.out.println("Error: Debe completar el campo!");
+            }else{
+                try{
+                    number = Integer.parseInt(entrance);
+                    
+                }
+            }
+
+        }
+    }
 }
--- src/TP0_P4/TP0_P4.java
+++ src/TP0_P4/TP0_P4.java
@@ -1,4 +1,13 @@
 package TP0_P4;
 
 public class TP0_P4 {
+
+    public static void main (String[] args){
+        System.out.println("### CALCULADORA ###");
+        System.out.println("1) Sumar");
+        System.out.println("2) Restar");
+        System.out.println("3) Multiplicar");
+        System.out.println("4) Dividir");
+
+    }
 }
