a3f456a506b8d5d7ac36bb79858fbe0f50ecda93
Pedro Cruz <pedrocruzsc2@gmail.com>
2025-08-25 12:06:13 -0300

    TP1 P3 Initialization

---
+++ src/TP1_P3/Licencia.java
@@ -0,0 +1,22 @@
+package TP1_P3;
+
+import java.util.Date;
+public class Licencia {
+
+    private int numeroIdentificacion;
+    private String tipoLicencia;
+    private Date fechaEmision;
+    private Date fechaVencimiento;
+
+    public Licencia(){
+        this.numeroIdentificacion = 000;
+        this.tipoLicencia = "a definir";
+        
+
+    }
+
+
+}
+
+
+// zerocool
